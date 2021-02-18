7f86720173e9729bd548bdb1fb13e40f64723e30
ikswi <59744173+ikswi@users.noreply.github.com>
2021-02-18 18:57:02 +0400

    Add files via upload

--- KrasnoeBeloe-main/AllProducts.Designer.cs
+++ KrasnoeBeloe-main/AllProducts.Designer.cs
@@ -32,25 +32,11 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.nameTB = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.pictureBox7 = new System.Windows.Forms.PictureBox();
             this.priceTB = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             this.SuspendLayout();
             // 
             // label5
@@ -89,50 +75,6 @@
             this.label2.TabIndex = 14;
             this.label2.Text = "1";
             // 
-            // pictureBox5
-            // 
-            this.pictureBox5.Image = global::qqqq.Properties.Resources.сухари3;
-            this.pictureBox5.Location = new System.Drawing.Point(494, 95);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(120, 120);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox5.TabIndex = 13;
-            this.pictureBox5.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Image = global::qqqq.Properties.Resources.пиво3;
-            this.pictureBox4.Location = new System.Drawing.Point(331, 95);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(120, 120);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox4.TabIndex = 12;
-            this.pictureBox4.TabStop = false;
-            this.pictureBox4.Visible = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.ErrorImage = global::qqqq.Properties.Resources.пиво1;
-            this.pictureBox3.Image = global::qqqq.Properties.Resources.сухари;
-            this.pictureBox3.InitialImage = global::qqqq.Properties.Resources.пиво1;
-            this.pictureBox3.Location = new System.Drawing.Point(169, 95);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(120, 120);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 11;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.ErrorImage = global::qqqq.Properties.Resources.пиво2;
-            this.pictureBox2.Image = global::qqqq.Properties.Resources.сухари2;
-            this.pictureBox2.Location = new System.Drawing.Point(11, 95);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(120, 120);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 10;
-            this.pictureBox2.TabStop = false;
-            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(260, 25);
@@ -149,30 +91,6 @@
             this.nameTB.Name = "nameTB";
             this.nameTB.Size = new System.Drawing.Size(100, 22);
             this.nameTB.TabIndex = 19;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(648, 95);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(121, 120);
-            this.pictureBox1.TabIndex = 20;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox6
-            // 
-            this.pictureBox6.Location = new System.Drawing.Point(10, 250);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(121, 120);
-            this.pictureBox6.TabIndex = 21;
-            this.pictureBox6.TabStop = false;
-            // 
-            // pictureBox7
-            // 
-            this.pictureBox7.Location = new System.Drawing.Point(180, 250);
-            this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(120, 120);
-            this.pictureBox7.TabIndex = 22;
-            this.pictureBox7.TabStop = false;
             // 
             // priceTB
             // 
@@ -207,28 +125,14 @@
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.priceTB);
-            this.Controls.Add(this.pictureBox7);
-            this.Controls.Add(this.pictureBox6);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.nameTB);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
             this.Name = "AllProducts";
             this.Text = "AllProducts";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -240,15 +144,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox nameTB;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox6;
-        private System.Windows.Forms.PictureBox pictureBox7;
         private System.Windows.Forms.TextBox priceTB;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label6;
--- KrasnoeBeloe-main/AllProducts.cs
+++ KrasnoeBeloe-main/AllProducts.cs
@@ -10,51 +10,102 @@ using System.Windows.Forms;
 
 namespace qqqq
 {
-//У нас будут персонажи
-        public struct Product
-        {
-            public PictureBox picture;
-            public string name;
-            public int price;
-        }
+    public struct Product
+    {
+        public PictureBox picture;
+        
+        public string name;
+        public string category;
+        public int price;
+    }
 
     public partial class AllProducts : Form
     {
-        
-        Product[] products_list = new Product[6];
+
+        Product[] products_list = new Product[600];
         public AllProducts()
         {
             InitializeComponent();
-            
-            products_list[0].picture = pictureBox2;
-            products_list[0].name = "Хрустеам сметана";
-            products_list[0].price = 60;
 
-            products_list[1].picture = pictureBox3;
-            products_list[1].name = "Хрустеам стейк";
-            products_list[1].price = 60;
+            //products_list[0].picture = pictureBox2;
+            products_list[0].name = "сухари2";
+            products_list[0].category = "Снэки";
+            products_list[0].price = 30;
 
-            products_list[2].picture = pictureBox3;
+            //products_list[1].picture = pictureBox3;
+            products_list[1].name = "сухари6";
+            products_list[1].category = "Снэки";
+            products_list[1].price = 30;
+
+            //products_list[2].picture = pictureBox3;
             products_list[2].name = "Хрустеам";
-            products_list[2].price = 60;
+            products_list[2].price = 30;
 
-            products_list[3].picture = pictureBox3;
-            products_list[3].name = "Хрустеам холодец";
-            products_list[3].price = 60;
+            //products_list[3].picture = pictureBox3;
+            products_list[3].name = "сухари3";
+            products_list[3].price = 50;
 
+            products_list[4].name = "пиво1";
+            products_list[4].category = "Напитки";
+            products_list[4].price = 60;
+
+            products_list[5].name = "пиво2";
+            products_list[5].category = "Напитки";
+            products_list[5].price = 61;
+
+            products_list[6].name = "пиво3";
+            products_list[6].category = "Напитки";
+            products_list[6].price = 62;
+
+            products_list[7].name = "пиво4";
+            products_list[7].category = "Напитки";
+            products_list[7].price = 63;
+
+            int x = 10;
+            int y = 100;
+            for (int i = 0; i < 8; i++)
+            {
+                PictureBox pb = new PictureBox();
+
+                try
+                {
+                    pb.Load("../../Resources/" + products_list[i].name + ".jpg");
+                }
+                catch (Exception) { }
+
+                pb.Location = new Point(x, y);
+                pb.Size = new Size(120, 120);
+                pb.SizeMode = PictureBoxSizeMode.Zoom;
+                //this.pictureBox2.Click += new System.EventHandler(pictureBox1_Click);
+                Controls.Add(pb);
+                products_list[i].picture = pb;
+
+                x = x + 160;
+                if (x + 120 > Width)
+                {
+                    y = y + 180;
+                    x = 10;
+                }
+            }
         }
+
+
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 5; i++)
             {
                 products_list[i].picture.Visible = true;
                 if (!products_list[i].name.Contains(nameTB.Text))
                     products_list[i].picture.Visible = false;
+
                 if (priceTB.Text != "" &&
                     products_list[i].price > Convert.ToInt32(priceTB.Text))
                     products_list[i].picture.Visible = false;
             }
         }
+
     }
 }
