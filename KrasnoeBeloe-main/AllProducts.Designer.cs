d94ea6d2b87957c3013f7f291bd7f48eb523b728
ikswi <59744173+ikswi@users.noreply.github.com>
2021-03-09 17:59:33 +0300

    Add files via upload

---
+++ KrasnoeBeloe-main/AllProducts.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace qqqq
+{
+    partial class AllProducts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.nameTB = new System.Windows.Forms.TextBox();
+            this.priceTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 45);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nameTB
+            // 
+            this.nameTB.Location = new System.Drawing.Point(12, 22);
+            this.nameTB.Name = "nameTB";
+            this.nameTB.Size = new System.Drawing.Size(100, 22);
+            this.nameTB.TabIndex = 19;
+            // 
+            // priceTB
+            // 
+            this.priceTB.Location = new System.Drawing.Point(12, 48);
+            this.priceTB.Name = "priceTB";
+            this.priceTB.Size = new System.Drawing.Size(100, 22);
+            this.priceTB.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Цена";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(132, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 17);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Наименование";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(872, 291);
+            this.panel1.TabIndex = 26;
+            // 
+            // AllProducts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 378);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.priceTB);
+            this.Controls.Add(this.nameTB);
+            this.Controls.Add(this.button1);
+            this.Name = "AllProducts";
+            this.Text = "AllProducts";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox nameTB;
+        private System.Windows.Forms.TextBox priceTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ KrasnoeBeloe-main/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qqqq
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AlkashkaClick(object sender, EventArgs e)
+        {
+            AlkashkaForm af = new AlkashkaForm("Напитки");
+            af.Show();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            AlkashkaForm af = new AlkashkaForm("Снэки");
+            af.Show();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AllProducts af = new AllProducts();
+            af.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
